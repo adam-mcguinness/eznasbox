4aefc2a8eeb11f86d91277497395da5d743b96fe
adam-mcguinness <ki4mor@gmail.com>
2024-06-18 13:40:16 -0600

    added global config for serialization

--- src/main/kotlin/local/plugins/Serialization.kt
+++ src/main/kotlin/local/plugins/Serialization.kt
@@ -1,11 +1,15 @@
 package local.plugins
 
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json{
+            encodeDefaults = true
+            ignoreUnknownKeys = true
+        })
     }
 }
